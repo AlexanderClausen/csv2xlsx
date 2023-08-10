e3ae1f08a079ad86263cc0e6242a1fbe8685add1
Alexander Clausen <git@alexanderclausen.com>
2023-08-10 16:09:47 +0200

    first commit

---
+++ home.py
@@ -0,0 +1,85 @@
+import streamlit as st
+import pandas as pd
+import zipfile
+import tempfile
+import os
+import base64
+from datetime import datetime
+
+# Upload comma-separated CSV file(s)
+uploaded_files = st.file_uploader("Choose a CSV file", accept_multiple_files=True, type=['csv', 'zip'])
+
+# Initiate data dictionary
+data = {}
+
+# Filename function
+def file_name():
+    return str(datetime.now().strftime("%Y-%m-%d_%H-%M_csv2xlsx_export"))
+
+# Iterate through each uploaded file and check if they are CSV or ZIP
+for uploaded_file in uploaded_files:
+    if uploaded_file.name.endswith('.csv'):
+        data[uploaded_file.name] = pd.read_csv(uploaded_file)
+
+    elif uploaded_file.name.endswith('.zip'):
+        with tempfile.TemporaryDirectory() as tmpdirname:
+            with zipfile.ZipFile(uploaded_file) as z:
+                z.extractall(tmpdirname)
+                for filename in z.namelist():
+                    if filename.endswith('.csv'):
+                        data[filename] = pd.read_csv(os.path.join(tmpdirname, filename))
+
+# Preview
+if len(data) > 0:
+    with st.expander('Preview data'):
+        preview_selector = st.selectbox('Preview', list(data.keys()))
+        st.dataframe(data[preview_selector])
+
+# Download
+if len(data) == 0:
+    st.info('Upload CSV or ZIP file(s).')
+elif len(data) > 1:
+    download_col1, download_col2 = st.columns(2)
+
+    with download_col1:
+        with st.expander('Option 1: Single file', expanded=True):
+            st.write("Download all data as a single XSLX file, with each CSV file as a separate sheet.")
+            # Download as single file with multiple sheets
+            with tempfile.TemporaryDirectory() as tmpdirname:
+                with pd.ExcelWriter(os.path.join(tmpdirname, f'{file_name()}.xlsx')) as writer:
+                    for key, value in data.items():
+                        # Create sheet name from filename, emitting the extension
+                        sheet_name = key[:-4][:31]  # Excel sheet name cannot be longer than 31 characters
+                        value.to_excel(writer, sheet_name=sheet_name, index=False)
+                with open(os.path.join(tmpdirname, f'{file_name()}.xlsx'), 'rb') as f:
+                    bytes_data = f.read()
+                b64 = base64.b64encode(bytes_data).decode()
+                href = f'data:file/xlsx;base64,{b64}'
+                st.download_button(label="Download", data=bytes_data, file_name=f'{file_name()}.xlsx', mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', use_container_width=True)
+
+    with download_col2:
+        with st.expander('Option 2: Separate files', expanded=True):
+            st.write("Download data as individual XLSX files with a single sheet, put together in a ZIP file.")
+            # Download as separate files in a ZIP
+            with tempfile.TemporaryDirectory() as tmpdirname:
+                with zipfile.ZipFile(os.path.join(tmpdirname, f'{file_name()}.zip'), 'w') as z:
+                    for key, value in data.items():
+                        xlsx_filename = f'{key[:-4]}.xlsx'
+                        value.to_excel(os.path.join(tmpdirname, xlsx_filename), index=False)
+                        z.write(os.path.join(tmpdirname, xlsx_filename), arcname=xlsx_filename)
+                with open(os.path.join(tmpdirname, f'{file_name()}.zip'), 'rb') as f:
+                    bytes_data = f.read()
+                b64 = base64.b64encode(bytes_data).decode()
+                href = f'data:file/zip;base64,{b64}'
+                st.download_button(label="Download", data=bytes_data, file_name=f'{file_name()}.zip', mime='application/zip', use_container_width=True)
+
+else:
+    # Download
+    with tempfile.TemporaryDirectory() as tmpdirname:
+        single_filename = list(data.keys())[0][:-4] + '.xlsx'
+        data[list(data.keys())[0]].to_excel(os.path.join(tmpdirname, single_filename), index=False)
+        with open(os.path.join(tmpdirname, single_filename), 'rb') as f:
+            bytes_data = f.read()
+        b64 = base64.b64encode(bytes_data).decode()
+        href = f'data:file/xlsx;base64,{b64}'
+        st.download_button(label="Download", data=bytes_data, file_name=single_filename, mime='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet', use_container_width=True)
